98a3e1ad46539abe837dd9a9f600062986644f1f
adriandcoding <theamr91@gmail.com>
2025-01-20 11:38:17 +0100

    update webpack configuration to copy images from public to dist and change dev server port to 9000

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -40,8 +40,8 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'public'), // Carpeta de origen
-          to: path.resolve(__dirname, 'dist'),    // Carpeta de destino
+          from: path.resolve(__dirname, 'public/images'), // Carpeta de origen
+          to: path.resolve(__dirname, 'dist/images'),    // Carpeta de destino
         },
       ],
     }),
@@ -50,7 +50,7 @@ module.exports = {
   devServer: {
     static: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9010,
+    port: 9000,
   },
   
   
