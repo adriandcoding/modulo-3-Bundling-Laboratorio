ebb0ecf5df133e3c1045e1297a13a5acd32f8384
adriandcoding <theamr91@gmail.com>
2025-01-20 17:07:07 +0100

    refactor webpack production configuration: remove unused output and module settings

--- Webpack/webpack.prod.js
+++ Webpack/webpack.prod.js
@@ -7,30 +7,6 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
   cache: {
     type: 'filesystem',
   },
