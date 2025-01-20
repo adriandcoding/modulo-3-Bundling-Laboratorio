99d783297f83b2c1492727c0b78d3347b64f6969
adriandcoding <theamr91@gmail.com>
2025-01-20 19:49:17 +0100

    replace webpack bundle analyzer configuration with performance-focused setup

---
+++ Webpack/webpack.perf.ts
@@ -0,0 +1,36 @@
+const { merge } = require("webpack-merge");
+const path = require("path");
+const common = require("./webpack.common.js");
+const { performancePlugins } = require("./webpack.perf.ts");
+const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+const fs = require("fs");
+
+const envPath = path.resolve(__dirname, ".env.production");
+const fileEnv = dotenv.parse(fs.readFileSync(envPath));
+
+const envKeys = {
+  ...fileEnv,
+  NODE_ENV: "production",
+};
+
+module.exports = merge(common, performancePlugins, {
+  mode: "production",
+  devtool: "source-map",
+  cache: {
+    type: "filesystem",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(envKeys),
+    }),
+  ],
+});
