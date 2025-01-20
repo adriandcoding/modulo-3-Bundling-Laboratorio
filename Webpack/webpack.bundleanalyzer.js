9773b625e225d3979401f34f847e3754235d49f0
adriandcoding <theamr91@gmail.com>
2025-01-20 19:42:44 +0100

    remove unused bundle analyzer import from webpack configuration

--- Webpack/webpack.bundleanalyzer.js
+++ Webpack/webpack.bundleanalyzer.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
 const path = require('path');
 const common = require('./webpack.common.js');
-const bundleAnalyzer = require('./webpack.bundleanalyzer.js');
 const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
