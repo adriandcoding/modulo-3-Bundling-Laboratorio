4a8d8e9f9edf678d62e6f485845076c4f140233a
adriandcoding <theamr91@gmail.com>
2025-01-20 17:12:42 +0100

    update webpack production configuration to load environment variables from .env.production

--- Webpack/webpack.prod.js
+++ Webpack/webpack.prod.js
@@ -3,6 +3,19 @@ const path = require('path');
 const common = require('./webpack.common.js');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+// Load .env.production file
+const envPath = path.resolve(__dirname, '.env.production');
+const fileEnv = dotenv.parse(fs.readFileSync(envPath));
+
+// Merge the environment variables with NODE_ENV set to 'production'
+const envKeys = {
+  ...fileEnv,
+  NODE_ENV: 'production',
+};
 
 module.exports = merge(common, {
   mode: 'production',
@@ -19,5 +32,8 @@ module.exports = merge(common, {
   },
   plugins: [
     new BundleAnalyzerPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(envKeys),
+    }),
   ],
 });
